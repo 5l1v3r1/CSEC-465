a7ffede06b738336996d1e20501d22e658352280
Jypper <castrojaylen@gmail.com>
2018-10-10 15:43:32 -0700

    Update DNS Enumeration.ps1
    Prints the hostname and the corresponding ip addresses

--- DNS Enumeration.ps1
+++ DNS Enumeration.ps1
@@ -4,6 +4,6 @@ $txtFile = Read-Host "Enter a file path"
 
 foreach($line in Get-Content $txtFile) {
 	if($line -match $regex) {
-		Resolve-DnsName $line | Format-Table -Property IPAddress
+		Resolve-DnsName $line | Format-Table -Property Name, IPAddress
 	}
 }
