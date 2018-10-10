e40b925bad75db434c74a169f002beb3ef5c8d2b
Jypper <castrojaylen@gmail.com>
2018-10-10 15:31:35 -0700

    Update DNS Enumeration.ps1
    Prints just ip addresses

--- DNS Enumeration.ps1
+++ DNS Enumeration.ps1
@@ -4,6 +4,6 @@ $txtFile = Read-Host "Enter a file path"
 
 foreach($line in Get-Content $txtFile) {
 	if($line -match $regex) {
-		Resolve-DnsName $line
+		Resolve-DnsName $line | Format-Table -Property IPAddress
 	}
 }
