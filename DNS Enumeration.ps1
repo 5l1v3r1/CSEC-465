edf100ed6360ca75124e4e3c179fa749ebe24442
Jypper <castrojaylen@gmail.com>
2018-10-10 09:20:00 -0700

    Update DNS Enumeration.ps1
    Forgot "$" before the i's so it now runs without crashing

--- DNS Enumeration.ps1
+++ DNS Enumeration.ps1
@@ -3,6 +3,6 @@
 $Text = Get-Content -Path txtFile.txt
 $Text.GetType() | Format-Table -AutoSize
 
-for(%i = 0; i -lt $name.length; i++) {
+for($i = 0; $i -lt $name.length; i++) {
 	Resolve-DnsName "Name: $name[i]"
 }
