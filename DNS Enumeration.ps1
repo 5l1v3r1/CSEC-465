a18dcc56831fc8d9f5e49683512b2ba575fa9a20
Jypper <castrojaylen@gmail.com>
2018-10-10 09:35:44 -0700

    Update DNS Enumeration.ps1
    Now takes user input for the file path

--- DNS Enumeration.ps1
+++ DNS Enumeration.ps1
@@ -1,6 +1,8 @@
 #DNS Enumeration
 
-foreach($line in Get-Content C:\Users\castr\txtFile.txt) {
+$txtFile = Read-Host "Enter a file path"
+
+foreach($line in Get-Content $txtFile) {
 	if($line -match $regex) {
 		Resolve-DnsName $line
 	}
