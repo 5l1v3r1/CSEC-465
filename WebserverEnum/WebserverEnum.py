ad95a4a008668b295890439741aa4c45e87df5bb
dphan <ducphanduy.agentp@gmail.com>
2018-10-11 00:55:11 -0400

    Finish WebServerScanner

--- WebserverEnum/WebserverEnum.py
+++ WebserverEnum/WebserverEnum.py
@@ -2,11 +2,27 @@ from __future__ import print_function
 import requests
 import ipaddress
 import sys
+import multiprocessing
+import subprocess
 
-scanTimeout = 1
-ports = range(1, 1000)
+pingTimeout = 0.1
+scanTimeout = 0.1
+#ports = [80, 443]
+ports = range(1000)
+nprocs = 10
+
+def scan(args):
+    port = args[1]
+    ip = args[0]
+#    print("Scanning " + str(args))
+    try:
+        r = requests.get('http://{}:{}'.format(ip, port), timeout=scanTimeout)
+        return port
+    except:
+        return None
 
 def main():
+
     if len(sys.argv) != 2:
         print("Usage: python {} <ip-range>|<ip-subnet>".format(sys.argv[0]))
         print("Example")
@@ -19,13 +35,17 @@ def main():
     elif sys.argv[1].find('/') != -1:
         network = ipaddress.ip_network(unicode(sys.argv[1]))
         for ip in network:
-            for port in ports:
-                try:
-                    print('scanning http://{}:{}'.format(ip, port))
-                    r = requests.get('http://{}:{}'.format(ip, port), timeout=scanTimeout)
-                    print('{}:{}'.format(ip, port))
-                except:
-                    pass
+            try:
+                subprocess.check_call('timeout {} ping -c 1 {} >/dev/null 2>&1'.format(pingTimeout, ip), shell=True)
+            except:
+                continue
+            print("Scanning " + str(ip))
+            pool = multiprocessing.Pool(processes=nprocs)
+            for res in pool.map(scan, [(str(ip), port) for port in ports]):
+                if res:
+                    print(res)
+            pool.close()
+            pool.join()
     elif sys.argv[1].find('-') != -1:
         ips = sys.argv[1].split('-')
         iprange = ipaddress.summarize_address_range(ipaddress.ip_address(unicode(ips[0])), ipaddress.ip_address(unicode(ips[1])))
@@ -33,12 +53,32 @@ def main():
             try:
                 network = iprange.next()
                 for ip in network:
-                    print(ip)
+                    try:
+                        subprocess.check_call('timeout {} ping -c 1 {} >/dev/null 2>&1'.format(pingTimeout, ip), shell=True)
+                    except:
+                        continue
+                    print("Scanning " + str(ip))
+                    pool = multiprocessing.Pool(processes=nprocs)
+                    for res in pool.imap_unordered(scan, [(str(ip), port) for port in ports]):
+                        if res:
+                            print(res)
+                    pool.close()
+                    pool.join()
             except:
                 break
     else:
-        print("unrecognized format")
-        return 1
+        ip = sys.argv[1]
+        try:
+            subprocess.check_call('timeout {} ping -c 1 {} >/dev/null 2>&1'.format(pingTimeout, ip), shell=True)
+        except:
+            pass
+        print("Scanning " + str(ip))
+        pool = multiprocessing.Pool(processes=nprocs)
+        for res in pool.imap_unordered(scan, [(str(ip), port) for port in ports]):
+            if res:
+                print(res)
+        pool.close()
+        pool.join()
 
 if __name__ == '__main__':
     main()
