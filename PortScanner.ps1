d227245dbeb2737a80c0bd6322765e56640fd195
Duc Phan <ducphanduy.agentp@gmail.com>
2018-10-10 22:44:17 -0400

    Add argument parse to PortScanner

--- PortScanner.ps1
+++ PortScanner.ps1
@@ -1,37 +1,106 @@
+# Getting IP range
+$ips = $args[0]
+$rangeIdx = $ips.ToString().IndexOf('-')
+$networkIdx = $ips.ToString().IndexOf('/')
 
-<#
-$iprange = Read-Host -prompt "Enter IP or IP range"
-if($iprange -contains "-")
-{
+If ($rangeIdx -ne -1 -and $networkIdx -ne -1) {
+    echo "Unrecognized IP format"
+} ElseIf($rangeIdx -ne -1) {
+    $ips = $ips.ToString().Split('-')
+    $startIP = ([IPAddress]$ips[0]).GetAddressBytes()
+    [Array]::Reverse($startIP)
+    $startIP = [uint32]([IPAddress]$startIP).Address
+    $endIP = ([IPAddress]$ips[1]).GetAddressBytes()
+    [Array]::Reverse($endIP)
+    $endIP = [uint32]([IPAddress]$endIP).Address
+} ElseIf($networkIdx -ne -1) {
+    $ips = $ips.ToString().Split('/')
+    $network = $ips[0]
+    $netmask = $ips[1]
+} Else {
+    $startIP = ([IPAddress]$ips).GetAddressBytes()
+    [Array]::Reverse($startIP)
+    $startIP = [uint32]([IPAddress]$startIP).Address
+    $endIP = ([IPAddress]$ips).GetAddressBytes()
+    [Array]::Reverse($endIP)
+    $endIP = [uint32]([IPAddress]$endIP).Address
+} 
 
+# Getting port range
+If ($args.Length -lt 2) {
+    echo "No ports specified"
+    exit(1)
 }
-$portrange = Read-Host -prompt "Enter port(s)"
-if($portrange -contains "-")
-{
-    $ports = $portrange.split("-")
-    
-}
-#>
+$portRangeIdx = $args[1].ToString().IndexOf('-')
+$portListIdx = $args[1].ToString().IndexOf(',')
 
-$ip = Read-Host -prompt "Enter IP"
-$port1 = Read-Host -prompt "Enter start of port range"
-$port2 = Read-Host -prompt "Enter end of port range"
-$port1i = [int]$port1
-$port2i = [int]$port2
-while($port1i -lt ($port2i + 1))
-{
-    try
-    {
-        $socket = New-Object System.Net.Sockets.TcpClient($ip, $port1i)
-        Write-Host "Port: ", [str]$port1i, " Open"
-        $socket.Close()
-	}
-	catch [System.Net.Sockets.SocketException]
-	{
-        continue
-	}
-    finally
-    {
-        $port1i++
+If ($portRangeIdx -ne -1) {
+    $portrange = $args[1].ToString().Split('-')
+    $startPort = [int]$portrange[0]
+    $endPort = [int]$portrange[1]
+} ElseIf ($portListIdx -ne -1) {
+    $portList = $args[1].ToString().Split(',')
+} Else {
+    $portListIdx = 0
+    $portList = $args[1]
+}
+
+$ip = [uint32]$startIP
+
+$result = "`nResult:`n"
+
+while($ip -lt ($endIP + 1)) {
+    $ip1 = ([IPAddress]$ip).GetAddressBytes()
+    [Array]::Reverse($ip1)
+    $ip1 = ([IPAddress]$ip1).IPAddressToString
+    ping -n 1 -w 100 $ip1 | Out-Null
+    If ($? -eq $False) {
+        echo "$ip1 is down"
+        $ip++
+        Continue
     }
+    echo "Scanning $ip1"
+    $resultString = "${ip1}: "
+    If ($portRangeIdx -ne -1) {
+        $port = $startPort
+
+        While($port -lt ($endPort + 1)) {
+            #echo "Scanning $port"
+            Try {
+                $socket = New-Object System.Net.Sockets.TcpClient($ip1, $port)
+                echo "Port $port is open"
+                $resultString += "$port "
+                $socket.Close()
+	        } Catch [System.Net.Sockets.SocketException] {
+                $errorCode = $_.Exception.InnerException.ErrorCode
+                If ($errorCode -eq 10061) {
+                    echo "Port $port is closed"
+                } ElseIf ($errorCode -eq 10060) {
+                    echo "Port $port is filtered"
+                }
+	        } Finally {
+                $port++
+            }
+        }
+    } ElseIf ($portListIdx -ne -1) {
+        foreach($port in $portList) {
+            #echo "Scanning $port"
+            Try {
+                $socket = New-Object System.Net.Sockets.TcpClient($ip1, $port)
+                echo "Port $port is open"
+                $resultString += "$port "
+                $socket.Close()
+	        } Catch [System.Net.Sockets.SocketException] {
+                $errorCode = $_.Exception.InnerException.ErrorCode
+                If ($errorCode -eq 10061) {
+                    echo "Port $port is closed"
+                } ElseIf ($errorCode -eq 10060) {
+                    echo "Port $port is filtered"
+                }
+	        }
+        }
+    }
+    $result += $resultString + "`n"
+    $ip++
 }
+echo $result
